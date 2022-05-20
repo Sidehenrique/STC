d48b494acc1076c96afc2945f150a378eb0604a8
SideHenrique <henriquesilver585@gmail.com>
2022-05-20 17:21:31 -0300

    validação com o banco

--- modulo/main.py
+++ modulo/main.py
@@ -1,70 +1,118 @@
+import pyodbc
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Login import Ui_MainLogin
 from Login import *
+import mod
 
 
 def main(ui):
-
+    # Tratamento do POPUP
     ui.frame_erro.hide()
+    ui.Button_quit.clicked.connect(lambda: ui.frame_erro.hide())
 
-    def a():
-        print(ui.lineEdit.text())
-    ui.ENTER.clicked.connect(a)
+    def checkPassword():
+        usuario = ui.lineEdit.text()
+        password = ui.lineEdit_3.text()
+        print(usuario, password)
+
+        try:
+            cursor = mod.conectar_mssql()
+            cursor.execute(f"""SELECT senha FROM cadastro WHERE usuario = '{usuario}';""")
+            senha_bd = cursor.fetchall()
+            print(senha_bd[0][0])
+            cursor.close()
+
+        except:
+            print('Usuário ou senha incorreto')
+            return
+
+        if password == senha_bd[0][0]:
+            print('Sucesso!!!')
+
+        else:
+            print('Senha incorreta')
+
+    ui.ENTER.clicked.connect(checkPassword)
 
 
-
-
-
-
+# def checkPassword():
+#     usuario = usuario
+#     password = password
 #
-# class MainWindow(QtWidgets.QMainWindow, Ui_MainLogin):
-#     def __init__(self):
-#         super(MainWindow, self).__init__()
-#         self.setupUi(self)
+#     try:
+#         cursor = mod.conectar_mssql()
+#         cursor.execute(f"""SELECT senha FROM cadastro WHERE usuario = '{usuario}';""")
+#         senha_bd = cursor.fetchall()
+#         print(senha_bd[0][0])
+#         cursor.close()
+#
+#     except:
+#         print('usuario ou senha invalidos')
+#
+#     if password == senha_bd[0][0]:
+#         print('Home')
+#
+#
 
 
-    # def checkFields(self):
-    #     textUser = ''
-    #     textPassword = ''
-    #
-    #     def showMessage(message):
-    #         self.frame_erro.show()
-    #         self.label_erro.setText(message)
-    #
-    #     # CHECAR USUÁRIO
-    #     if not self.lineEdit.text():
-    #         textUser = ' User Empyt. '
-    #     else:
-    #         textUser = ''
-    #
-    #     # CHECAR PASSWORD
-    #     if not self.lineEdit_3.text():
-    #         textPassword = ' Password Empyt. '
-    #     else:
-    #         textPassword = ''
-    #
-    #     # CHECK FIELDS
-    #     if textUser + textPassword != '':
-    #         text = textUser + textPassword
-    #         showMessage(text)
-    #     else:
-    #         text = ' Login OK. '
-    #         if self.checkBox.isChecked():
-    #             text = text + ' | Saver user: OK '
-    #         showMessage(text)
-    #
-    #     # POPUP BUTTON
-    #     self.Button_quit.clicked.connect(lambda: self.frame_erro.hide())
-    #
-    #     # INICIAR POPUP OCULTAMENTE
-    #     self.frame_erro.hide()
-    #
-    #     # BUTTON LOGIN
-    #     self.ENTER.clicked.connect(self.checkFields)
+# Usuario e Senha
+# User = ui.lineEdit.text()
+# Password = ui.lineEdit_3.text()
+#
+#
+# ui.ENTER.clicked.connect()
+#
+#
+# def checkPassword(User, Password):
+#     textUser = ''
+#     textPassword = ''
+#
+#     if textUser and textPassword != '':
+#         print('Usuário ou Senha invalido')
 
+
+# def checkFields(self):
+#     textUser = ''
+#     textPassword = ''
+#
+#     def showMessage(message):
+#         self.frame_erro.show()
+#         self.label_erro.setText(message)
+#
+#     # CHECAR USUÁRIO
+#     if not self.lineEdit.text():
+#         textUser = ' User Empyt. '
+#     else:
+#         textUser = ''
+#
+#     # CHECAR PASSWORD
+#     if not self.lineEdit_3.text():
+#         textPassword = ' Password Empyt. '
+#     else:
+#         textPassword = ''
+#
+#     # CHECK FIELDS
+#     if textUser + textPassword != '':
+#         text = textUser + textPassword
+#         showMessage(text)
+#     else:
+#         text = ' Login OK. '
+#         if self.checkBox.isChecked():
+#             text = text + ' | Saver user: OK '
+#         showMessage(text)
+#
+#     # POPUP BUTTON
+#     self.Button_quit.clicked.connect(lambda: self.frame_erro.hide())
+#
+#     # INICIAR POPUP OCULTAMENTE
+#     self.frame_erro.hide()
+#
+#     # BUTTON LOGIN
+#     self.ENTER.clicked.connect(self.checkFields)
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainLogin = QtWidgets.QMainWindow()
     ui = Ui_MainLogin()
---
+++ modulo/mod.py
@@ -0,0 +1,27 @@
+import pyodbc
+
+
+# MANIPULANDO BANCO DE DADOS SQL ---------------------------------------------------
+
+def conectar_mssql():
+    con = pyodbc.connect(
+        # Driver que será utilizado na conexão
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        # IP ou nome do servidor.
+        'SERVER=10.6.32.24;'
+        # Porta
+        'PORT=1433;'
+        # Banco que será utilizado.
+        'DATABASE=base_stc;'
+        # Nome de usuário.
+        f'UID=sa;'
+        # Senha.
+        f'PWD=sicoob;')
+
+    # Criando o cursor que irá executar os comandos SQL (instruções DML, DDL, etc).
+    cur = con.cursor()
+    return cur
+
+# MANIPULANDO PUPUP ----------------------------------------------------------------
+
+def
