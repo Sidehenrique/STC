5cb2571833914c0760cbc7133fc2bc7cc5de6452
SideHenrique <henriquesilver585@gmail.com>
2022-05-24 12:58:30 -0300

    Chamando tela Home

--- modulo/main.py
+++ modulo/main.py
@@ -1,15 +1,18 @@
-import pyodbc
-from PyQt5 import QtCore, QtGui, QtWidgets
-from Login import Ui_MainLogin
 from Login import *
+from Home import *
 import mod
 
 
 def main(ui):
-    # Tratamento do POPUP
+    # Tratamento do POPUP -----------------------------------------------------------------
     ui.frame_erro.hide()
     ui.Button_quit.clicked.connect(lambda: ui.frame_erro.hide())
 
+    def mensage(mensagem):
+        ui.frame_erro.show()
+        ui.label_erro.setText(mensagem)
+
+    # VALIDAÇÃO DE DADOS -----------------------------------------------------------------
     def checkPassword():
         usuario = ui.lineEdit.text()
         password = ui.lineEdit_3.text()
@@ -23,100 +26,46 @@ def main(ui):
             cursor.close()
 
         except:
-            print('Usuário ou senha incorreto')
+            texto1 = 'Usuário ou senha incorreto'
+            mensage(texto1)
             return
 
         if password == senha_bd[0][0]:
+            texto2 = 'Login efetuado com sucesso'
+            mensage(texto2)
             print('Sucesso!!!')
+            MainLogin.close()
+            MainWindow.show()
+
+
+
 
         else:
-            print('Senha incorreta')
+            texto3 = 'A senha esta incorreta'
+            mensage(texto3)
 
     ui.ENTER.clicked.connect(checkPassword)
 
 
-# def checkPassword():
-#     usuario = usuario
-#     password = password
-#
-#     try:
-#         cursor = mod.conectar_mssql()
-#         cursor.execute(f"""SELECT senha FROM cadastro WHERE usuario = '{usuario}';""")
-#         senha_bd = cursor.fetchall()
-#         print(senha_bd[0][0])
-#         cursor.close()
-#
-#     except:
-#         print('usuario ou senha invalidos')
-#
-#     if password == senha_bd[0][0]:
-#         print('Home')
-#
-#
+def home(mw):
+    # Tratamento Botão sair -----------------------------------------------------------
+    def getLogin():
+        MainWindow.close()
+        MainLogin.show()
+    mw.pushButton_sair.clicked.connect(getLogin)
 
-
-# Usuario e Senha
-# User = ui.lineEdit.text()
-# Password = ui.lineEdit_3.text()
-#
-#
-# ui.ENTER.clicked.connect()
-#
-#
-# def checkPassword(User, Password):
-#     textUser = ''
-#     textPassword = ''
-#
-#     if textUser and textPassword != '':
-#         print('Usuário ou Senha invalido')
-
-
-# def checkFields(self):
-#     textUser = ''
-#     textPassword = ''
-#
-#     def showMessage(message):
-#         self.frame_erro.show()
-#         self.label_erro.setText(message)
-#
-#     # CHECAR USUÁRIO
-#     if not self.lineEdit.text():
-#         textUser = ' User Empyt. '
-#     else:
-#         textUser = ''
-#
-#     # CHECAR PASSWORD
-#     if not self.lineEdit_3.text():
-#         textPassword = ' Password Empyt. '
-#     else:
-#         textPassword = ''
-#
-#     # CHECK FIELDS
-#     if textUser + textPassword != '':
-#         text = textUser + textPassword
-#         showMessage(text)
-#     else:
-#         text = ' Login OK. '
-#         if self.checkBox.isChecked():
-#             text = text + ' | Saver user: OK '
-#         showMessage(text)
-#
-#     # POPUP BUTTON
-#     self.Button_quit.clicked.connect(lambda: self.frame_erro.hide())
-#
-#     # INICIAR POPUP OCULTAMENTE
-#     self.frame_erro.hide()
-#
-#     # BUTTON LOGIN
-#     self.ENTER.clicked.connect(self.checkFields)
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
     MainLogin = QtWidgets.QMainWindow()
+    MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainLogin()
+    mw = Ui_MainWindow()
+    mw.setupUi(MainWindow)
     ui.setupUi(MainLogin)
     MainLogin.show()
     main(ui)
+    home(mw)
     sys.exit(app.exec_())
---
+++ modulo/teste.py
@@ -0,0 +1,7 @@
+import pyodbc
+from PyQt5 import QtCore, QtGui, QtWidgets
+from Login import Ui_MainLogin
+from Login import *
+import mod
+
+
